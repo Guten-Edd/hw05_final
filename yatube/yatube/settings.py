bb57472e6dab3a2f1773221725be1094ba2586ed
Эдуард Соловьев <eddsolovei@gmail.com>
2022-11-24 15:15:15 +0300

    first attempt_flake

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 LOGIN_URL = 'users:login'
@@ -136,4 +136,4 @@ CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
     }
-} 
+}
